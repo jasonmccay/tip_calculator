2ff227ce6115f42cd602d595038407ee953394cb
Jason McCay <jason.mccay@gmail.com>
2014-10-04 11:18:23 -0500

    fixed formatting. also removed parenthesis

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -8,12 +8,18 @@ tip = 17
 
 # calculate expenses
 
-tax_value = meal * (tax / 100)
+tax_value = meal.to_f * tax/100
 meal_with_tax = tax_value.to_f + meal
-tip_value = meal_with_tax * (tip / 100)
+tip_value = meal_with_tax * tip / 100
 total_cost = meal_with_tax + tip_value.to_f
 
-puts "The pre-tax cost of your meal was: $#{meal}."
-puts "At #{tax}%, tax for this meal is: $#{tax_value}."
-puts "For a #{tip}% tip, you should leave $#{tip_value}."
-puts "The grand total for this meal is then: $#{total_cost}."
+puts " "
+puts "-------------------------------------------------------"
+puts " " 
+print "The pre-tax cost of your meal was: $%.2f.\n" % meal
+print "At %d%%, tax for this meal is: $%.2f.\n" % [tax, tax_value]
+print "For a %d%% tip, you should leave $%.2f.\n" % [tip, tip_value]
+print "The grand total for this meal is then: $%.2f\n" % total_cost
+puts " "
+puts "--------------------------------------------------------"
+puts " "

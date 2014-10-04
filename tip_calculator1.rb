596c7a0fe4431db41e6cf49f2bf06122952f6c70
Jason McCay <jason.mccay@gmail.com>
2014-10-04 10:47:01 -0500

    changed prints to puts

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -13,7 +13,7 @@ meal_with_tax = tax_value.to_f + meal
 tip_value = meal_with_tax * (tip / 100)
 total_cost = meal_with_tax + tip_value.to_f
 
-print "The pre-tax cost of your meal was: $#{meal}."
-print "At #{tax}%, tax for this meal is: $#{tax_value}."
-print "For a #{tip}% tip, you should leave $#{tip_value}."
-print "The grand total for this meal is then: $#{total_cost}."
+puts "The pre-tax cost of your meal was: $#{meal}."
+puts "At #{tax}%, tax for this meal is: $#{tax_value}."
+puts "For a #{tip}% tip, you should leave $#{tip_value}."
+puts "The grand total for this meal is then: $#{total_cost}."
